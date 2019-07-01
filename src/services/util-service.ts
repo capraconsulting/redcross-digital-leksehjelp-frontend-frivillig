39a33d87ac5a54f0f657bfb1c8552c0761771fae
mbu <mbu@capraconsulting.no>
2019-07-01 11:03:02 +0200

    LEKSE-92/Add response when not in grades

--- src/services/util-service.ts
+++ src/services/util-service.ts
@@ -1,6 +1,9 @@
 import GRADES from '../grades';
 
 export const studentGradeFormat = (studentGrade: string): string => {
+  if (parseInt(studentGrade) < 8 || parseInt(studentGrade) > 13) {
+    return studentGrade;
+  }
   return GRADES.filter(({ gradeID }) => gradeID === studentGrade).map(
     ({ label }) => label,
   )[0];
