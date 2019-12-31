729bb599f02fdaca92a1bf02e2771a62c994799a
sandraskars <sandraskarshaug@gmail.com>
2019-12-31 13:32:13 +0100

    Create AdminRoute which checks if user is admin to restrict admin paths

--- src/router.tsx
+++ src/router.tsx
@@ -14,6 +14,7 @@ import {
   AdminUsersContainer,
   ProfileContainer,
 } from './containers';
+import AdminRoute from './components/AdminRoute';
 
 interface IProps {
   onLogout(): void;
@@ -37,9 +38,9 @@ const Routes = ({ onLogout }: IProps) => {
             />
           )}
         />
-        <Route exact path="/admin/users" component={AdminUsersContainer} />
-        <Route
-          exaxt
+        <AdminRoute exact path="/admin/users" component={AdminUsersContainer} />
+        <AdminRoute
+          exact
           path="/admin/questions"
           component={AdminQuestionsContainer}
         />
